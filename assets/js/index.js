d0517f1acc6d4a543bc9d5c3cb9a7df4038caeb8
abdussamiakanda <abdussamiakanda@gmail.com>
2022-04-07 17:01:57 +0600

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,19 @@
 function goTo(path){
   window.location.assign(path);
 }
+
+var btns = ['btn0','btn1','btn2','btn3','btn4'];
+
+var divs = ['overview','outline','notes','videos','reg'];
+
+function show(btn){
+  for(var i in btns){
+    if(btns[i] === btn){
+      document.getElementById(btns[i]).classList.add('selected');
+      document.getElementById(divs[i]).classList.remove('hide');
+    } else {
+      document.getElementById(btns[i]).classList.remove('selected');
+      document.getElementById(divs[i]).classList.add('hide');
+    }
+  }
+}
