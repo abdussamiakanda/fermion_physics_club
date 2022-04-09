0a2caea4d866febf9bfffb360cea720234775d99
abdussamiakanda <abdussamiakanda@gmail.com>
2022-04-10 02:20:21 +0600

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,18 @@ function show(btn){
     }
   }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    renderMathInElement(document.body, {
+      // customised options
+      // • auto-render specific keys, e.g.:
+      delimiters: [
+          {left: '$$', right: '$$', display: true},
+          {left: '$', right: '$', display: false},
+          {left: '\\(', right: '\\)', display: false},
+          {left: '\\[', right: '\\]', display: true}
+      ],
+      // • rendering keys, e.g.:
+      throwOnError : false
+    });
+});
