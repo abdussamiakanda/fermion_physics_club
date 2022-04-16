95b337cce77ba65ddfa0eeeaf56077f333b640ce
abdussamiakanda <abdussamiakanda@gmail.com>
2022-04-17 03:53:14 +0600

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,22 @@ function goTo(path){
   window.location.assign(path);
 }
 
+var btns = ['btn0','btn1','btn2','btn3','btn4'];
+
+var divs = ['overview','outline','notes','videos','reg'];
+
+function show(btn){
+  for(var i in btns){
+    if(btns[i] === btn){
+      document.getElementById(btns[i]).classList.add('selected');
+      document.getElementById(divs[i]).classList.remove('hide');
+    } else {
+      document.getElementById(btns[i]).classList.remove('selected');
+      document.getElementById(divs[i]).classList.add('hide');
+    }
+  }
+}
+
 var gallery = document.querySelector('#gallery');
 var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
 var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
@@ -41,22 +57,6 @@ gallery.querySelectorAll('.gallery-item').forEach(function (item) {
 });
 
 
-
-var btns = ['btn0','btn1','btn2','btn3','btn4'];
-
-var divs = ['overview','outline','notes','videos','reg'];
-
-function show(btn){
-  for(var i in btns){
-    if(btns[i] === btn){
-      document.getElementById(btns[i]).classList.add('selected');
-      document.getElementById(divs[i]).classList.remove('hide');
-    } else {
-      document.getElementById(btns[i]).classList.remove('selected');
-      document.getElementById(divs[i]).classList.add('hide');
-    }
-  }
-}
 
 document.addEventListener("DOMContentLoaded", function() {
     renderMathInElement(document.body, {
